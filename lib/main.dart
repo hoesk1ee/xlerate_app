5758c992f19008a1789ce535f298673a7c9eba82
hoesk1ee <hoeskiee@gmail.com>
2026-07-14 23:05:54 +0700

    Ref: Add formatting and several files

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World'))),
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
     );
   }
 }
