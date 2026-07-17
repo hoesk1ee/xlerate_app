785423d5a783ad7888c0e77825a30c89a3d526bb
hoeskiee <hoeskiee@gmail.com>
2026-07-18 01:24:57 +0700

    Ref: Fix double main function

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,4 @@ class MyApp extends StatelessWidget {
       home: const FeedbackPage(),
     );
   }
-}import 'package:flutter/material.dart';
-import 'presentation/pages/feedback_page.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Feedback Demo',
-      home: const FeedbackPage(),
-    );
-  }
 }
